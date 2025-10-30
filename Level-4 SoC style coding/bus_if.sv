4f07375af94d94fc1002ba48ecf55849521cc645
Pathakamuri Naresh <123228297+PathNaresh@users.noreply.github.com>
2025-10-30 12:47:47 +0530

    Create bus_if.sv

---
+++ Level-4 SoC style coding/bus_if.sv
@@ -0,0 +1,14 @@
+
+import bus_pkg::*;
+
+interface bus_if #(parameter ADDR_WIDTH=8, DATA_WIDTH=32)(input logic clk);
+  
+  logic valid;
+  logic ready;
+  logic wr_en;
+  logic [ADDR_WIDTH-1:0] addr;
+  logic [DATA_WIDTH-1:0] wdata;
+  logic [DATA_WIDTH-1:0] rdata;
+  resp_t resp;
+  
+endinterface

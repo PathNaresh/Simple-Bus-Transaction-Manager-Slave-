12ff747737a06e38958d29df3dfd7b703f044f57
Pathakamuri Naresh <123228297+PathNaresh@users.noreply.github.com>
2025-10-30 11:57:03 +0530

    Create top_tb.sv

---
+++ top_tb.sv
@@ -0,0 +1,17 @@
+
+`timescale 1ns/1ps
+
+module top_tb;
+  
+  logic clk = 0;
+  always #5 clk = ~clk;  // 100MHz clock
+
+  bus_if bus(clk);
+  manager m0(bus);
+  slave   s0(bus);
+
+  initial begin
+    #100 $finish;
+  end
+  
+endmodule
